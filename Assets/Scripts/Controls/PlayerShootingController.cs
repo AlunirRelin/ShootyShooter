5fa6419c5015a020883b5e55da0ab6ab6194b78a
hydrationGoblinStudios <raphael.vconsidera@al.infnet.edu.br>
2023-08-29 22:11:45 -0300

    videogamews arma trocas

--- Assets/Scripts/Controls/PlayerShootingController.cs
+++ Assets/Scripts/Controls/PlayerShootingController.cs
@@ -6,6 +6,7 @@ using Mirror;
 
 public class PlayerShootingController : NetworkBehaviour
 {
+    public GameObject[] gunObject;
     public Gun gun;
     public void Update()
     {
@@ -14,6 +15,16 @@ public class PlayerShootingController : NetworkBehaviour
         {
             gun.FireCooldown = Time.time + 1f / gun.fireRate;
             gun.Shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && gunObject[0].activeSelf)
+        {
+            gunObject[0].SetActive(false); gunObject[1].SetActive(true);
+            gun = gunObject[1].GetComponent<Gun>();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1) && gunObject[1].activeSelf)
+        {
+            gunObject[1].SetActive(false); gunObject[0].SetActive(true);
+            gun = gunObject[0].GetComponent<Gun>();
         }
     }
 }
